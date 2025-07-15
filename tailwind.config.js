57f27bff1da437852c4fa5589933af6070c56147
Dennis0101 <67768185+Dennis0101@users.noreply.github.com>
2025-07-15 15:50:07 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#121212",
+        secondary: "#1e1e1e",
+        accent: "#26a69a"
+      }
+    }
+  },
+  plugins: []
 }
